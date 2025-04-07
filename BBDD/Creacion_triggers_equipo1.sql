ed90666d92729af97531921708d50aa2b64c29d3
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-04-07 10:14:24 +0200

    Agregar triggers

--- BBDD/Creacion_triggers_equipo1.sql
+++ BBDD/Creacion_triggers_equipo1.sql
@@ -14,61 +14,76 @@ RAISE_APPLICATION_ERROR(-20099, v_mensaje);
 
 END trg_valida_ganador_Equipo01;
 
-/*NO MÁS DE 6 JUGADORES*/
+/*NO Mï¿½S DE 6 JUGADORES*/
 CREATE OR REPLACE TRIGGER max_jugadores_equipo
 BEFORE INSERT ON JUGADORES
 FOR EACH ROW
 
 DECLARE
     total_jugadores NUMBER;
-    v_error VARCHAR2(250);
+    v_mensaje VARCHAR2(250);
     v_exceso_jugadores EXCEPTION;
-    
     
 BEGIN
     SELECT COUNT(*) INTO total_jugadores
     FROM JUGADORES
     WHERE cod_equipo = :NEW.cod_equipo;
     
-    IF total_jugadores >= 6 THEN
+    IF total_jugadores > 6 THEN
         RAISE v_exceso_jugadores;
     END IF;
     
 EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20020, 'Error: no se encontraron datos.');
-
-    WHEN TOO_MANY_ROWS THEN
-        RAISE_APPLICATION_ERROR(-20030, 'Error inesperado.');
-        
     WHEN v_exceso_jugadores THEN
-        RAISE_APPLICATION_ERROR(-20002, 'No se pueden registrar más de seis jugadores por equipo.');
-
+        RAISE_APPLICATION_ERROR(-20002, 'No se pueden registrar mï¿½s de seis jugadores por equipo.');
     WHEN OTHERS THEN
-        v_error_msg := 'Error Oracle: ' || TO_CHAR(SQLCODE) || ', ' || SQLERRM;
-        RAISE_APPLICATION_ERROR(-20000, v_error);
+        v_mensaje := 'Error Oracle: ' || TO_CHAR(SQLCODE) || ', ' || SQLERRM;
+        RAISE_APPLICATION_ERROR(-20000, v_mensaje);
 END max_jugadores_equipo;  
 
 
 /*SALARIO MINIMO SMI*/
 CREATE OR REPLACE TRIGGER salario_minimo
-BEFORE INSERT ON jugadores
-FOR EACH ROW
-
-BEGIN
-
-IF (sueldo < 1184)THEN
-raise_application_error(-20000, 'El salario no puede ser menor del SMI');
-END IF;
-
-EXCEPTION
-WHEN NO_DATA_FOUND THEN
-NULL;
-
-WHEN TOO_MANY_ROWS THEN
-raise_application_error(-20500,'No se puede insertar el salario.');
-
-WHEN OTHERS THEN
-raise_application_error(-20100,'Error desconocido.');
-
+    BEFORE INSERT ON jugadores
+    FOR EACH ROW
+    BEGIN
+        IF (:new.sueldo < 1184) THEN
+            :new.sueldo := 1184;
+        END IF;
+    
+    EXCEPTION
+        WHEN OTHERS THEN
+            v_mensaje := 'Err. desconocido, ' || TO_CHAR(SQLCODE) || SQLERRM;
+            RAISE_APPLICATION_ERROR(-20099, v_mensaje);
+    
 END salario_minimo;
+
+/*Validacion al crear calendario todos los equipo tienen mas de 2 jugadores*/
+CREATE OR REPLACE TRIGGER minJugadoresPorEquipo
+    BEFORE INSERT ON enfrentamientos
+    FOR EACH ROW
+    DECLARE
+        numJugEquipo1 NUMBER;
+        numJugEquipo2 NUMBER;
+        v_mensaje VARCHAR2(255);
+        e_menosDe2Jugadores EXCEPTION;
+    BEGIN
+        SELECT COUNT(*) INTO numJugEquipo1
+        FROM jugadores
+        WHERE cod_equipo = :NEW.equipo1;
+    
+        SELECT COUNT(*) INTO numJugEquipo2
+        FROM jugadores
+        WHERE cod_equipo = :NEW.equipo2;
+    
+        IF numJugEquipo1 < 2 OR numJugEquipo2 < 2 THEN
+            RAISE e_menosDe2Jugadores;
+        END IF;
+    
+    EXCEPTION
+        WHEN e_menosDe2Jugadores THEN
+            RAISE_APPLICATION_ERROR(-20020, 'Uno o ambos equipos tienen menos de 2 jugadores.');
+        WHEN OTHERS THEN
+            v_mensaje := 'Err. desconocido, ' || TO_CHAR(SQLCODE) || SQLERRM;
+            RAISE_APPLICATION_ERROR(-20099, v_mensaje);
+END minJugadoresPorEquipo;
