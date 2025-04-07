5fe35ac749559fdefbdc3061bdb8d18d7ed89fe0
KoldoSoriguren <koldo.soriguren@ikasle.egibide.org>
2025-04-07 10:00:45 +0200

    Update Creacion_triggers_equipo1.sql

--- BBDD/Creacion_triggers_equipo1.sql
+++ BBDD/Creacion_triggers_equipo1.sql
@@ -3,18 +3,21 @@ BEFORE INSERT OR UPDATE ON enfrentamientos
 FOR EACH ROW
 declare
 v_mensaje varchar2(250);
+e_equipos exception;
 BEGIN
     IF :NEW.ganador != :NEW.equipo1 AND :NEW.ganador != :NEW.equipo2 THEN
-        RAISE_APPLICATION_ERROR(-20002, 'El equipo ganador debe ser el equipo1 o el equipo2 del enfrentamiento.');
+      raise e_equipos;
     END IF;
 EXCEPTION
 WHEN OTHERS THEN
 v_mensaje:= 'Error desconcido ' || to_char(SQLCODE) ||  SQLERRM;
 RAISE_APPLICATION_ERROR(-20099, v_mensaje);
-
+when e_equipos then
+v_mensaje:= 'El equipo ganador debe ser el equipo1 o el equipo2 del enfrentamiento.';
+ RAISE_APPLICATION_ERROR(-20002,v_mensaje);
 END trg_valida_ganador_Equipo01;
 
-/*NO M¡S DE 6 JUGADORES*/
+/*NO M√ÅS DE 6 JUGADORES*/
 CREATE OR REPLACE TRIGGER max_jugadores_equipo
 BEFORE INSERT ON JUGADORES
 FOR EACH ROW
@@ -42,7 +45,7 @@ EXCEPTION
         RAISE_APPLICATION_ERROR(-20030, 'Error inesperado.');
         
     WHEN v_exceso_jugadores THEN
-        RAISE_APPLICATION_ERROR(-20002, 'No se pueden registrar m·s de seis jugadores por equipo.');
+        RAISE_APPLICATION_ERROR(-20002, 'No se pueden registrar m√°s de seis jugadores por equipo.');
 
     WHEN OTHERS THEN
         v_error_msg := 'Error Oracle: ' || TO_CHAR(SQLCODE) || ', ' || SQLERRM;
