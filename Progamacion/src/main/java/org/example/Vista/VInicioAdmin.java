ec151228adf35e581b8a448682d05dd73082d8ac
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-04-08 18:23:46 +0200

    Crear ventana Inicio Admin

---
+++ Progamacion/src/main/java/org/example/Vista/VInicioAdmin.java
@@ -0,0 +1,16 @@
+package org.example.Vista;
+
+import javax.swing.*;
+
+public class VInicioAdmin {
+    private JPanel pPrincipal;
+    private JPanel pHeader;
+    private JPanel pBody;
+    private JButton bAdministrar;
+    private JButton bCerrarEtapa;
+    private JButton bVerInforme;
+    private JButton bGenerarCalendario;
+    private JButton bIntroducirResultados;
+    private JPanel pArriba;
+    private JPanel pAbajo;
+}
