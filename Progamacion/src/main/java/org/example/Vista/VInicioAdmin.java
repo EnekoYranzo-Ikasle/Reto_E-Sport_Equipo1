1fdce8c7d03b8ecc6e33f514c74ff25b9727a990
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-04-09 12:53:14 +0200

    Hacer ventanas Admin

--- Progamacion/src/main/java/org/example/Vista/VInicioAdmin.java
+++ Progamacion/src/main/java/org/example/Vista/VInicioAdmin.java
@@ -3,10 +3,12 @@ package org.example.Vista;
 import org.example.Controlador.VistaController;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VInicioAdmin extends JFrame {
     private VistaController vistaController;
+    private Login login;
 
     private JPanel pPrincipal;
     private JPanel pHeader;
@@ -18,14 +20,33 @@ public class VInicioAdmin extends JFrame {
     private JButton bIntroducirResultados;
     private JPanel pArriba;
     private JPanel pAbajo;
+    private JButton bLogOut;
 
-    public VInicioAdmin(VistaController vistaController) {
+    public VInicioAdmin(VistaController vistaController, Login login) {
         this.vistaController = vistaController;
+        this.login = login;
 
         setContentPane(pPrincipal);
         setTitle("Vista Admin");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 400);
         setLocationRelativeTo(null);
+
+        bLogOut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                login.setVisible(true);
+                dispose();
+            }
+        });
+
+        bAdministrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VAdministrarAdmin vAdministrarAdmin = new VAdministrarAdmin(vistaController);
+                vAdministrarAdmin.setVisible(true);
+                dispose();
+            }
+        });
     }
 }
