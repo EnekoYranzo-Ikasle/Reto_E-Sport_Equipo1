a2d777c5fad44a7de688d4d26f0780ff4a2c3174
Bouya-Ahmed <bouyaahmed.chrifbouya@ikasle.egibide.org>
2025-04-09 13:07:43 +0200

    Nomenclatura.

--- BBDD/CargaDeDatos-Equipo1.sql
+++ BBDD/CargaDeDatos-Equipo1.sql
@@ -14,22 +14,22 @@ INSERT INTO equipos (nombre, fechaFundacion)
     VALUES ('Equipo Beta', TO_DATE('15-08-2012', 'DD-MM-YYYY'));
 
 -- Inserción de datos en la tabla JUGADORES
-INSERT INTO jugadores (nombre, apellido, nacionalidad, fechaNacimiento, 
-    nickname, rol, sueldo, cod_equipo) 
+INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+    nickname, rol, sueldo, codEquipo) 
     VALUES ('Juan', 'Pérez', 'Española', TO_DATE('12-03-1995', 'DD-MM-YYYY'), 
         'juanito', 'duelista', 1500, 1);
 
-INSERT INTO jugadores (nombre, apellido, nacionalidad, fechaNacimiento, 
-    nickname, rol, sueldo, cod_equipo) 
+INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+    nickname, rol, sueldo, codEquipo) 
     VALUES ('Carlos', 'Gomez', 'Española', TO_DATE('22-07-1997', 'DD-MM-YYYY'), 
         'carlitos', 'controlador', 1200, 1);
 
-INSERT INTO jugadores (nombre, apellido, nacionalidad, fechaNacimiento, 
-    nickname, rol, sueldo, cod_equipo) 
+INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+    nickname, rol, sueldo, codEquipo) 
     VALUES ('Lucas', 'Méndez', 'Méxicana', TO_DATE('30-11-1996', 'DD-MM-YYYY'), 
         'lucas', 'iniciador', 1600, 2);
 
-INSERT INTO jugadores (nombre, apellido, nacionalidad, fechaNacimiento, 
-    nickname, rol, sueldo, cod_equipo) 
+INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+    nickname, rol, sueldo, codEquipo) 
     VALUES ('Raúl', 'Sánchez', 'Colombiana', TO_DATE('02-04-1998', 'DD-MM-YYYY'), 
         'raulito', 'centinela', 1100, 2);
