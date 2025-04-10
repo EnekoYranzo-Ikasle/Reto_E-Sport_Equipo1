112a71cabd3eb473c9dcf788652956bcac28a9d4
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-04-10 14:03:59 +0200

    Diseñar dialogo de mostrar jugadores

--- Progamacion/src/main/java/org/example/Vista/DVisualizarEquipos.java
+++ Progamacion/src/main/java/org/example/Vista/DVisualizarEquipos.java
@@ -5,14 +5,20 @@ import org.example.Controlador.VistaController;
 import javax.swing.*;
 
 public class DVisualizarEquipos extends JDialog {
-    private JPanel contentPane;
+    private JPanel pPrincipal;
+    private JPanel pHeader;
+    private JPanel pBody;
+    private JTextArea taMostrar;
+    private JButton bVolver;
     private JButton buttonOK;
-    private JButton buttonCancel;
     private VistaController vistaController;
     public DVisualizarEquipos(VistaController vistaController) {
         this.vistaController = vistaController;
-        setContentPane(contentPane);
+
+        setContentPane(pPrincipal);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+
+
     }
 }
