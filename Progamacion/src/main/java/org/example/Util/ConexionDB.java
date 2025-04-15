74c88f689b8a87fb3cff084fdc88adb0d7cb4eb0
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-04-15 08:15:13 +0200

    Cambiar conexion

--- Progamacion/src/main/java/org/example/Util/ConexionDB.java
+++ Progamacion/src/main/java/org/example/Util/ConexionDB.java
@@ -10,15 +10,15 @@ public class ConexionDB {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             // Para usar en clase
-//            String url = "jdbc:oracle:thin:@172.20.225.114:1521:orcl";
-//            String user = "eqdaw01";
-//            String password = "eqdaw01";
+            String url = "jdbc:oracle:thin:@172.20.225.114:1521:orcl";
+            String user = "eqdaw01";
+            String password = "eqdaw01";
 
             // Para usar la maquina virtual
-            String url = "jdbc:oracle:thin:@127.0.0.1:1521:orclcdb";
-            String user = "system";
-            String password = "oracle";
-            conn = DriverManager.getConnection(url, user, password);
+//            String url = "jdbc:oracle:thin:@127.0.0.1:1521:orclcdb";
+//            String user = "system";
+//            String password = "oracle";
+//            conn = DriverManager.getConnection(url, user, password);
 
         }catch (ClassNotFoundException e) {
             System.out.println("Error en Class.forName " + e.getMessage());
