f73febc9894369d2250c2a0b28c0a1f81d0a17e8
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-04-22 14:40:13 +0200

    Cambiar GENERATED ALWAYS AS DEFALUT por sequencias.

--- BBDD/CargaDeDatos-Equipo1.sql
+++ BBDD/CargaDeDatos-Equipo1.sql
@@ -1,35 +1,38 @@
 -- Inserción de datos en la tabla USUARIOS
-INSERT INTO usuarios (email, password, tipo) 
-    VALUES('user@email.com', '12345', 'user');
+INSERT INTO usuarios (id, email, password, tipo) 
+    VALUES (sec_codUsuarios.NEXTVAL, 'user@email.com', '12345', 'user');
 
--- Inserción de datos en la tabla USUARIOS
-INSERT INTO usuarios (email, password, tipo) 
-    VALUES('admin@email.com', '12345', 'admin');
+INSERT INTO usuarios (id, email, password, tipo) 
+    VALUES (sec_codUsuarios.NEXTVAL, 'admin@email.com', '12345', 'admin');
 
--- Inserción de datos en la tabla EQUIPOS
-INSERT INTO equipos (nombre, fechaFundacion) 
-    VALUES ('Equipo Alpha', TO_DATE('20-05-2010', 'DD-MM-YYYY'));
-
-INSERT INTO equipos (nombre, fechaFundacion) 
-    VALUES ('Equipo Beta', TO_DATE('15-08-2012', 'DD-MM-YYYY'));
-
--- Inserción de datos en la tabla JUGADORES
-INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+-- Inserción de datos en la tabla EQUIPOS para el equipo 1
+INSERT INTO equipos (codEquipo, nombre, fechaFundacion) 
+    VALUES (sec_codEquipo.NEXTVAL, 'Equipo Alpha', TO_DATE('20-05-2010', 'DD-MM-YYYY'));
+    
+-- Inserción de datos en la tabla JUGADORES para el equipo 1
+INSERT INTO jugadores (codJugador, nombre, apellidos, nacionalidad, fechaNacimiento, 
     nickname, rol, sueldo, codEquipo) 
-    VALUES ('Juan', 'Pérez', 'Española', TO_DATE('12-03-1995', 'DD-MM-YYYY'), 
-        'juanito', 'duelista', 1500, 3);
+    VALUES (sec_codJugadores.NEXTVAL, 'Juan', 'Pérez', 'Española', TO_DATE('12-03-1995', 'DD-MM-YYYY'), 
+        'juanito', 'duelista', 1500, sec_codEquipo.CURRVAL);
 
-INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+INSERT INTO jugadores (codJugador, nombre, apellidos, nacionalidad, fechaNacimiento, 
     nickname, rol, sueldo, codEquipo) 
-    VALUES ('Carlos', 'Gomez', 'Española', TO_DATE('22-07-1997', 'DD-MM-YYYY'), 
-        'carlitos', 'controlador', 1200, 3);
+    VALUES (sec_codJugadores.NEXTVAL, 'Carlos', 'Gomez', 'Española', TO_DATE('22-07-1997', 'DD-MM-YYYY'), 
+        'carlitos', 'controlador', 1200, sec_codEquipo.CURRVAL);
 
-INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
-    nickname, rol, sueldo, codEquipo) 
-    VALUES ('Lucas', 'Méndez', 'Méxicana', TO_DATE('30-11-1996', 'DD-MM-YYYY'), 
-        'lucas', 'iniciador', 1600, 4);
+-- Inserción de datos en la tabla EQUIPOS para el equipo 2
+INSERT INTO equipos (codEquipo, nombre, fechaFundacion) 
+    VALUES (sec_codEquipo.NEXTVAL, 'Equipo Beta', TO_DATE('15-08-2012', 'DD-MM-YYYY'));
 
-INSERT INTO jugadores (nombre, apellidos, nacionalidad, fechaNacimiento, 
+-- Inserción de datos en la tabla JUGADORES para el equipo 2
+INSERT INTO jugadores (codJugador, nombre, apellidos, nacionalidad, fechaNacimiento, 
     nickname, rol, sueldo, codEquipo) 
-    VALUES ('Raúl', 'Sánchez', 'Colombiana', TO_DATE('02-04-1998', 'DD-MM-YYYY'), 
-        'raulito', 'centinela', 1100, 4);
+    VALUES (sec_codJugadores.NEXTVAL, 'Lucas', 'Méndez', 'Méxicana', TO_DATE('30-11-1996', 'DD-MM-YYYY'), 
+        'lucas', 'iniciador', 1600, sec_codEquipo.CURRVAL);
+
+INSERT INTO jugadores (codJugador, nombre, apellidos, nacionalidad, fechaNacimiento, 
+    nickname, rol, sueldo, codEquipo) 
+    VALUES (sec_codJugadores.NEXTVAL, 'Raúl', 'Sánchez', 'Colombiana', TO_DATE('02-04-1998', 'DD-MM-YYYY'), 
+        'raulito', 'centinela', 1100, sec_codEquipo.CURRVAL);
+
+COMMIT;
